c071d0133d57762e6184a040940cd9a343c96017
ankitc <ankitc>
2013-02-14 00:16:53 +0000

    changes to cache to change the input params and add some error testing

--- python/filemgmt/cache.py
+++ python/filemgmt/cache.py
@@ -91,74 +91,85 @@ class Cache(object):
         print runsite_root
         print "cache Dictionary from Table: "
         print cacheDict
-        self.put_within_job(file_list,runsite_root,cacheDict)
+        retCodeDict = self.put_within_job(file_list,cacheDict)
+        print "the final return"
+        print retCodeDict
 
 
-    def put_within_job(self,file_list,runsite_root,cacheDict):
+    def put_within_job(self,file_list,cacheDict):
         """ the function is used to put files into cache from within an already running job.
         A list is provided and each file is put into the cache directory. 
         the location table for cache directory is then inserted new rows or the previously existing rows are updated
         on successful transfer of the file
+        the file_list has the structure: {'source':'destination'}
+        {'red/D00157536_z_c62_r6p01_scix.fits': 'OPS/finalcut/201206r6/D00157536/red'}
         """ 
 
         """create a regular expression for the root directory of the runsite. 
         this will be removed from the path of source file to get the relative path which will be created inside the cache directory
         """
-        runsite_root_canonicalized = os.path.realpath(runsite_root)
+        # Don't need this anymore. we don't need to bother with finding the absolute path of the file. runsite_root_canonicalized = os.path.realpath(runsite_root)
         returnCodeDict = {}
+        dataBaseInsertDict = {}
         #print "the runsite is " + os.path.realpath(runsite_root)
         
         onTargetMethod = cacheDict['on_target']
-        for file in file_list:
+        #for file in file_list:
+        #for filesource, fileDestWithinCache in file_list:
+        for fileSource,fileDestWithinCache in file_list.iteritems():
            # print "putting file" + file
-            """if the source filepath is relative, convert it into absolute path
-               we do this so that the absolute path can be easily used to copy files from one location to another 
-               without confusion
-            """
-            if not os.path.isabs(os.path.realpath(file)):
-                file = runsite_root + '/' + os.path.realpath(file)
-            else:
-                file = os.path.realpath(file)
             """create the absolute destination path for the location in cache for the incoming file.
              this is done by adding the root of cache location to the relative path of the source file"""
             #print "another attempt: " + os.path.realpath(file).replace(os.path.realpath(runsite_root),'')
 
-            destination = cacheDict['root'] + os.path.realpath(file).replace(runsite_root_canonicalized,'')
+            destination = cacheDict['root'] + '/' + fileDestWithinCache
+            if not os.path.exists(destination):
+                os.makedirs(destination)
             #print "the destination filepath is %s" % destination
+
             """
             call transportFile function and store the results in to a dictionary.
             this dictionary will be used later to update the database
             """
-            fileName = os.path.basename(file)
-            "initialize the returnCodeDict[fileName] dictionary"
 
-            returnCodeDict[fileName] = {}
-            returnCodeDict[fileName]['transfer_status'] = self.transportFile(file,destination,onTargetMethod)
-            returnCodeDict[fileName]['filename'] = fileName
-            returnCodeDict[fileName]['path'] = os.path.dirname(destination) #os.path.realpath(os.path.dirname(file))
-            returnCodeDict[fileName]['cache_name'] = cacheDict['name']
-            returnCodeDict[fileName]['site_name'] = cacheDict['sitename']
+            fileName = os.path.basename(fileSource)
+
+            retErrorMsg = self.transportFile(fileSource,destination,onTargetMethod)
+            if retErrorMsg == '':
+                dataBaseInsertDict[fileName] = {}
+                dataBaseInsertDict[fileName]['filename'] = fileName
+                dataBaseInsertDict[fileName]['path'] = os.path.dirname(destination)
+                dataBaseInsertDict[fileName]['cache_name'] = cacheDict['name']
+                dataBaseInsertDict[fileName]['site_name'] = cacheDict['sitename']
+
+            else:
+                returnCodeDict[fileName] = retErrorMsg
         
 #        print "calling DB"
 #        print returnCodeDict    
-        self.logTransportedFilesInDB(returnCodeDict)
-        return
+        self.logTransportedFilesInDB(dataBaseInsertDict)
+        return returnCodeDict
     
     def logTransportedFilesInDB(self,fileDict):
         
         insertArr = []
+        selectArr = []
         p = re.compile("(\.fits)(\.fz)?")
         for file, fileDets in fileDict.iteritems():
-            #print "looping over" + file
-            if fileDets['transfer_status'] is 0:
-                file = p.sub("",file)
-               # print "inserting file" + file
-                insertArr.append([file,fileDets['path'],fileDets['site_name'],fileDets['cache_name']])
+            print "looping over" + file
+            #if fileDets['transfer_status'] is 0:
+            "remove the fits and fz from the filename"
+            file = p.sub("",file)
+            # print "inserting file" + file
+            insertArr.append([file,fileDets['path'],fileDets['site_name'],fileDets['cache_name']])
         #print "the insert arr"
         #print insertArr    
 
         sqlInsert = "insert into ops_cache_location  values (:1,:2,:3,:4) " 
-        #sqlMerge = "merge into ops_cache_location A using (select :1 as file from dual ) B on ( A.filename = B.file) WHEN NOT MATCHED THEN INSERT  (A.filename, A.path, A.archive_node_name, A.cache_name values (:2,:3,:4,:5) "  # insertArr
+#        merge into ops_cache_location A using (select 'five' as fl from dual ) B on ( A.filename = B.fl) WHEN NOT MATCHED THEN INSERT  (A.filename, A.path, A.archive_node_name, A.cache_name) values ('one','two','three','four')
+        #sqlMerge = "merge into ops_cache_location A using (select 'dec021593_11_scamp' as fl from dual ) B on ( A.filename = B.fl) WHEN NOT MATCHED THEN INSERT  (A.filename, A.path, A.archive_node_name, A.cache_name values (':2',':3',':4',':5') "  # insertArr
+#        sqlMerge = "merge into ops_cache_location A using (select 'dec021593_11_scamp' as fl from dual ) B on ( A.filename = B.fl) WHEN NOT MATCHED THEN INSERT  (A.filename, A.path, A.archive_node_name, A.cache_name values (':1',':2',':3',':4') "  # insertArr
+        #self._cur.executemany(sqlInsert,insertArr)
         self._cur.executemany(sqlInsert,insertArr)
         self._dbh.commit()
         #print "the final insertArr is "
@@ -201,20 +212,20 @@ class Cache(object):
             print "did not transfer file. we are not ready for that yet"
         
         #if return != '':
-            print "there was an error in transporting file error  %s " %(returnStats)
-            
-        return returnCode
+        #print "the returnStat for runCommand is " 
+        return returnStats[1]
     
     def pre_put (self,list,cache_name):
         "comments go here"
         return
-    def get_within_job_wrapper(self,fileList,cache_name,site_name):
+    def get_within_job_wrapper(self,fileList,cache_name):
         """
         this function will get the files from a cache and put them into the runsite directory. 
         Inputs are an array of files with their complete path to which they need to be moved
         """
         colList = {}#['name','on_target','not_on_target','root','cache_name']
         fileNameDict = {}
+        returnDict = {}
         fileStr = ""
         p = re.compile("(\.fits)(\.fz)?")
         for filePath in fileList:
@@ -222,20 +233,23 @@ class Cache(object):
             fileBaseName = p.sub("",fileBaseName)
             fileStr = fileStr + "'" + fileBaseName + "', "
             fileNameDict[fileBaseName] = filePath 
+            returnDict[fileBaseName] = "Did not find the file in the database"
         "remove the last 2 characters"    
         fileStr = "(" + fileStr[:-2] + ")" 
         
         """get the files which are registered in cache. 
         this means that the files should be present in cache (unless some scientist deletes them by hand ;) )"""
         sqlQueryForFilesInCache = "select path,filename from ops_cache_location where filename in " +  fileStr
-#        print "sql to get files" + sqlQueryForFilesInCache
+        #print "sql to get files" + sqlQueryForFilesInCache
         resultFilesInCache = fileutils.get_queryresult_dictionay(sqlQueryForFilesInCache,self._cur)
         #print "the files in cache are"
         #print resultFilesInCache
         for key in resultFilesInCache:
-         #   print "the filename is: " 
             tempFileName = key['filename']
-          #  print tempFileName
+            #print "deleting the filename is: " 
+            #print tempFileName
+            "delete the returnDict files which were found in the database"
+            del returnDict[tempFileName]
             key['destination_path']  =   fileNameDict[tempFileName]
         #print "the files in cache AFTER are"
         #print resultFilesInCache
@@ -247,46 +261,40 @@ class Cache(object):
         cacheInfoDict = fileutils.get_queryresult_dictionay(sqlGetCacheInfo,self._cur)
 #        print " the query sqlGetCacheInfo %s returned %s" % (sqlGetCacheInfo,cacheInfoDict)
         
-        """
-        get the information about the runsite from the database
-        """
-        sqlGetSiteInfo = "select * from ops_data_defs where type = 'runsite' and name = '%s'" % site_name
+       # sqlGetSiteInfo = "select * from ops_data_defs where type = 'runsite' and name = '%s'" % site_name
         #print sqlGetSiteInfo
-        resultSite = fileutils.get_queryresult_dictionay(sqlGetSiteInfo,self._cur)
-        runsite_root = resultSite[0]['root']
+       # resultSite = fileutils.get_queryresult_dictionay(sqlGetSiteInfo,self._cur)
+        #runsite_root = resultSite[0]['root']
 #        print "inputs for get_within_job filesDictionary: "
 #        print resultFilesInCache
 #        print "runsite root: "
 #        print runsite_root
 #        print "cache table information: "
 #        print cacheInfoDict[0]
-        self.get_within_job(resultFilesInCache,runsite_root,cacheInfoDict[0])
+        returnDictJob = self.get_within_job(resultFilesInCache,cacheInfoDict[0])
+        for retFileName,retStatus in returnDictJob.iteritems(): 
+            returnDict[retFileName] = retStatus
 
-        return
+        #print "the final return is "
+        #print returnDict
+        return returnDict
     
-    def get_within_job(self,file_list,runsite_root,cacheInfoDict):
+    def get_within_job(self,file_list,cacheInfoDict):
         """
            This will be removed from the path of source file to get the relative path which will be created inside the cache directory
         """
         returnCodeDict = {}
-        runsite_root_canonicalized = os.path.realpath(runsite_root)
-        #print "the runsite is " + os.path.realpath(runsite_root)
         
         onTargetMethod = cacheInfoDict['on_target']
-        returnDict = []
+        returnDict = {}
         for filePath in file_list:
             """if the destination filepath is relative, convert it into absolute path
                we do this so that the absolute path can be easily used to copy files from one location to another 
                without confusion
             """
             #print "looping over file 1"
-            #print filePath['path']
-            if not os.path.isabs(os.path.realpath(filePath['path'])):
-                "if the filePath provided is relative, add the runsite root to it to make it an absolute path"
-                canonicalizedDestinationFilePath = os.path.realpath(runsite_root_canonicalized) + '/' + os.path.realpath(filePath['destination_path'])
-            else:
-                " if the path is absolute, leave it alone, only change the path to canonicalized"
-                canonicalizedDestinationFilePath = os.path.realpath(filePath['destination_path'])
+            #print filePath
+            canonicalizedDestinationFilePath = os.path.realpath(filePath['destination_path'])
                 
             """create the absolute source path for the file
              this is done by adding the root of cache location to the path given in ops_cache_location filename and adding the filename in the end
@@ -295,13 +303,12 @@ class Cache(object):
             if not os.path.isabs(canonicalizedfilePathInCache):
 #                print "the filepath is not abs: " + canonicalizedfilePathInCache
                 canonicalizedfilePathInCache = cacheInfoDict['root'] + '/' + canonicalizedfilePathInCache
-                canonicalizedSourcePath = os.path.realpath(cacheInfoDict['root'] + '/' + canonicalizedfilePathInCache + '/' + filePath['filename'])
+                canonicalizedSourcePath = os.path.realpath(cacheInfoDict['root'] + '/' + canonicalizedfilePathInCache + '/' + os.path.basename(filePath['destination_path']))
             else:
 #                print "canonicalizedfilePathInCache is abs: " + canonicalizedfilePathInCache
-                canonicalizedSourcePath = os.path.realpath(canonicalizedfilePathInCache + '/' + filePath['filename'])
+                canonicalizedSourcePath = os.path.realpath(canonicalizedfilePathInCache + '/' + os.path.basename(filePath['destination_path']))
             
 
-            #destination = runsite_root + "/" + os.path.realpath(file).replace(os.path.realpath(resultDict['root']),'')
             #print "the destination filepath is %s" % destination
             """
             call transportFile function and store the results in to a dictionary.
@@ -309,8 +316,9 @@ class Cache(object):
             """
             fileName = os.path.basename(filePath['filename'])
 
-            if self.transportFile(canonicalizedSourcePath,canonicalizedDestinationFilePath,onTargetMethod) is not 0:
-                returnDict.append(filePath)
+            retStatus =  self.transportFile(canonicalizedSourcePath,canonicalizedDestinationFilePath,onTargetMethod)
+            if retStatus != '':
+                returnDict[fileName] = retStatus
             
         return returnDict
             
@@ -328,6 +336,7 @@ class Cache(object):
         """runCommand is used to run a system command. input is the command to be run. It returns the stdout and stderr texts."
         stdout and stderr should be empty for a successful command execution"""
         #if verbose: print cmd
+        #print "running command " + cmd
         p = subprocess.Popen(cmd, shell=True,stdin= open("/dev/null"), 
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
         (stdouttxt, stderrtxt) = p.communicate()
@@ -340,9 +349,13 @@ class Cache(object):
 #### Embedded simple test
 if __name__ ==  '__main__' :
     cache = Cache()
-    list = ['/home/ankitc/work/cache_test_runsite/dec021593_11_scamp.fits','/home/ankitc/work/cache_test_runsite/DES2219-4147_g.fits.fz']
-    cache.put_within_job_wrapper(list,'destest_cache','destest_site')
+    #list = ['/home/ankitc/work/cache_test_runsite/dec021593_11_scamp.fits','/home/ankitc/work/cache_test_runsite/DES2219-4147_g.fits.fz']
+    list = {}
+    list2 = {}
+    list = {'/home/ankitc/work/cache_test_runsite/dec021593_11_scamp.fits' : 'random','/home/ankitc/work/cache_test_runsite/DES2219-4147_g.fits.fdfd' : 'random/random1'}
+    list2 = {'/home/ankitc/work/cache_test_runsite/dec021593_11_scamp.fits','/home/ankitc/work/cache_test_runsite/DES2219-4147_g.fits.fdfd'}
+   # cache.put_within_job_wrapper(list,'destest_cache','destest_site')
 
-    #cache.get_within_job_wrapper(list,'destest_cache','destest_site')
+    cache.get_within_job_wrapper(list2,'destest_cache')
 #    print dbh.get_column_names('archive_sites')
 
