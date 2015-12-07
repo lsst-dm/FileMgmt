6e4b1e1d82ff0b6832f7cda0da8ecf47ffe6a951
Michelle Gower <mgower@illinois.edu>
2015-12-07 16:26:23 +0000

    Convert Makefile to setup.py, update FileMgmt.table file with commented out dependencies

---
+++ setup.py
@@ -0,0 +1,21 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*")
+etc_files = glob.glob("etc/*")
+
+# The main call
+setup(name='FileMgmt',
+      version ='2.0.0',
+      license = "GPL",
+      description = "DESDM's file management framework",
+      author = "Michelle Gower",
+      author_email = "mgower@illinois.edu",
+      packages = ['filemgmt'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/FileMgmt.table']),
+                  ('etc',etc_files)]
+      )
+
