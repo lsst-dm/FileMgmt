11eafcd1aa47f9d39c878d5a5879b9ba78fa1480
Gregory Daues <daues@illinois.edu>
2016-01-30 14:14:55 +0000

    Getting ready for version 2.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ etc_files = glob.glob("etc/*")
 
 # The main call
 setup(name='FileMgmt',
-      version ='2.0.2',
+      version ='2.0.3',
       license = "GPL",
       description = "DESDM's file management framework",
       author = "Michelle Gower",
